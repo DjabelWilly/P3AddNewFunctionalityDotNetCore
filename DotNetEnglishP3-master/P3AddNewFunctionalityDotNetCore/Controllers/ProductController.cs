73dfe9e93ebd74e412c63c0053a4733bb828f100
willy djabelkhir <willy.djabelkhir@office.weda.fr>
2025-06-24 11:39:07 +0200

    add comments + script lib jquery

--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Controllers/ProductController.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Controllers/ProductController.cs
@@ -18,24 +18,48 @@ namespace P3AddNewFunctionalityDotNetCore.Controllers
             _languageService = languageService;
         }
 
+        /// <summary>
+        /// Displays a list of all products.
+        /// </summary>
+        /// <returns>The Index view with the list of products.</returns>
         public IActionResult Index()
         {
             IEnumerable<ProductViewModel> products = _productService.GetAllProductsViewModel();
             return View(products);
         }
 
+        /// <summary>
+        /// Displays a list of all products to authorized users in descending order by ID.
+        /// Used for administrative purposes.
+        /// </summary>
+        /// <returns>The Admin view with the ordered list of products.</returns>
         [Authorize]
         public IActionResult Admin()
         {
             return View(_productService.GetAllProductsViewModel().OrderByDescending(p => p.Id));
         }
 
+
+        /// <summary>
+        /// Displays the form for creating a new product.
+        /// Only accessible to authorized users.
+        /// </summary>
+        /// <returns>The Create view.</returns>
         [Authorize]
         public ViewResult Create()
         {
             return View();
         }
 
+
+        /// <summary>
+        /// Handles the POST request to create a new product. 
+        /// Validates the product using the product service, adds any validation errors to the ModelState, 
+        /// and saves the product if the model is valid. 
+        /// Redirects to the Admin page upon successful creation, or returns the form view with validation errors otherwise.
+        /// </summary>
+        /// <param name="product">The product data submitted from the form.</param>
+        /// <returns>A redirect to the Admin action if successful; otherwise, the Create view with validation messages.</returns>
         [Authorize]
         [HttpPost]
         public IActionResult Create(ProductViewModel product)
@@ -58,6 +82,12 @@ namespace P3AddNewFunctionalityDotNetCore.Controllers
             }
         }
 
+        /// <summary>
+        /// Handles the POST request to delete a product by its ID.
+        /// Calls the product service to remove the product and then redirects to the Admin view.
+        /// </summary>
+        /// <param name="id">The ID of the product to delete.</param>
+        /// <returns>A redirect to the Admin action after deletion.</returns>
         [Authorize]
         [HttpPost]
         public IActionResult DeleteProduct(int id)
--- DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/Services/ProductService.cs
+++ DotNetEnglishP3-master/P3AddNewFunctionalityDotNetCore/Models/Services/ProductService.cs
@@ -10,6 +10,10 @@ using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
 
 namespace P3AddNewFunctionalityDotNetCore.Models.Services
 {
+    /// <summary>
+    /// Service responsible for managing products, including retrieving,
+    /// validating, updating stock quantities, saving, and deleting products.
+    /// </summary>
     public class ProductService : IProductService
     {
         private readonly ICart _cart;
@@ -25,13 +29,23 @@ namespace P3AddNewFunctionalityDotNetCore.Models.Services
             _orderRepository = orderRepository;
             _localizer = localizer;
         }
+
+        /// <summary>
+        /// Retrieves all products and maps them to view models for display purposes.
+        /// </summary>
+        /// <returns>A list of ProductViewModel instances.</returns>
         public List<ProductViewModel> GetAllProductsViewModel()
         {
-             
+
             IEnumerable<Product> productEntities = GetAllProducts();
             return MapToViewModel(productEntities);
         }
 
+        /// <summary>
+        /// Maps a collection of Product entities to a list of ProductViewModel objects.
+        /// </summary>
+        /// <param name="productEntities">The collection of Product entities.</param>
+        /// <returns>A list of ProductViewModel objects.</returns>
         private static List<ProductViewModel> MapToViewModel(IEnumerable<Product> productEntities)
         {
             List <ProductViewModel> products = new List<ProductViewModel>();
@@ -51,36 +65,62 @@ namespace P3AddNewFunctionalityDotNetCore.Models.Services
             return products;
         }
 
+        /// <summary>
+        /// Retrieves all products from the repository.
+        /// </summary>
+        /// <returns>A list of Product entities.</returns>
         public List<Product> GetAllProducts()
         {
             IEnumerable<Product> productEntities = _productRepository.GetAllProducts();
             return productEntities?.ToList();
         }
 
+        /// <summary>
+        /// Retrieves a single product by its ID and maps it to a ProductViewModel.
+        /// </summary>
+        /// <param name="id">The product ID.</param>
+        /// <returns>The corresponding ProductViewModel.</returns>
         public ProductViewModel GetProductByIdViewModel(int id)
         {
             List<ProductViewModel> products = GetAllProductsViewModel().ToList();
             return products.Find(p => p.Id == id);
         }
 
-
-        public Product GetProductById(int id)
+        /// <summary>
+        /// Retrieves a single product entity by its ID.
+        /// </summary>
+        /// <param name="id">The product ID.</param>
+        /// <returns>The corresponding Product entity.</returns>
+         public Product GetProductById(int id)
         {
             List<Product> products = GetAllProducts().ToList();
             return products.Find(p => p.Id == id);
         }
 
+        /// <summary>
+        /// Asynchronously retrieves a single product by ID from the repository.
+        /// </summary>
+        /// <param name="id">The product ID.</param>
+        /// <returns>The corresponding Product entity.</returns>
         public async Task<Product> GetProduct(int id)
         {
             var product = await _productRepository.GetProduct(id);
             return product;
         }
 
+        /// <summary>
+        /// Asynchronously retrieves all products from the repository.
+        /// </summary>
+        /// <returns>A list of Product entities.</returns>
         public async Task<IList<Product>> GetProduct()
         {
             var products = await _productRepository.GetProduct();
             return products;
         }
+
+        /// <summary>
+        /// Updates the stock quantities of all products based on the contents of the shopping cart.
+        /// </summary>
         public void UpdateProductQuantities()
         {
             Cart cart = (Cart) _cart;
@@ -90,6 +130,11 @@ namespace P3AddNewFunctionalityDotNetCore.Models.Services
             }
         }
 
+        /// <summary>
+        /// Validates the fields of a ProductViewModel and returns a list of localized error messages.
+        /// </summary>
+        /// <param name="product">The product view model to validate.</param>
+        /// <returns>A list of validation error messages.</returns>
         // TODO this is an example method, remove it and perform model validation using data annotations
         public List<string> CheckProductModelErrors(ProductViewModel product)
         {
@@ -132,12 +177,21 @@ namespace P3AddNewFunctionalityDotNetCore.Models.Services
             return modelErrors;
         }
 
+        /// <summary>
+        /// Saves a new product to the repository after mapping it from a view model.
+        /// </summary>
+        /// <param name="product">The product view model to save.</param>
         public void SaveProduct(ProductViewModel product)
         {
             var productToAdd = MapToProductEntity(product);
             _productRepository.SaveProduct(productToAdd);
         }
 
+        /// <summary>
+        /// Maps a ProductViewModel to a Product entity.
+        /// </summary>
+        /// <param name="product">The view model to convert.</param>
+        /// <returns>The corresponding Product entity.</returns>
         private static Product MapToProductEntity(ProductViewModel product)
         {
             Product productEntity = new Product
@@ -151,6 +205,10 @@ namespace P3AddNewFunctionalityDotNetCore.Models.Services
             return productEntity;
         }
 
+        /// <summary>
+        /// Deletes a product by its ID and removes it from the cart if present.
+        /// </summary>
+        /// <param name="id">The ID of the product to delete.</param>
         public void DeleteProduct(int id)
         {
             // TODO what happens if a product has been added to a cart and has been later removed from the inventory ?
